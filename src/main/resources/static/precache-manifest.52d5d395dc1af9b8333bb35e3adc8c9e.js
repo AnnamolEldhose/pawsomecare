1e0abfb4d04bb63e896f03eaa3fb2fe14cbc9fb1
Annamol Eldhose <annaeldhose455@gmail.com>
2024-08-16 22:11:03 -0400

    first commit

---
+++ src/main/resources/static/precache-manifest.52d5d395dc1af9b8333bb35e3adc8c9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37dae3e4173530499a5d60de57bc9d88",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4166a17bbf30d4528403",
+    "url": "./static/css/2.896c4c41.chunk.css"
+  },
+  {
+    "revision": "b71dcb9640fd1f18eb8f",
+    "url": "./static/css/main.d08decd9.chunk.css"
+  },
+  {
+    "revision": "4166a17bbf30d4528403",
+    "url": "./static/js/2.2ca48c54.chunk.js"
+  },
+  {
+    "revision": "41b549c17a676bfb90e223837ed2109b",
+    "url": "./static/js/2.2ca48c54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b71dcb9640fd1f18eb8f",
+    "url": "./static/js/main.2016a74b.chunk.js"
+  },
+  {
+    "revision": "be327e4c013980ebb586",
+    "url": "./static/js/runtime-main.1f58792a.js"
+  }
+]);
